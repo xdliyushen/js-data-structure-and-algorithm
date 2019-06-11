9c896e340917ee08f82f5f8d858c3ac98fc9925f
李雨珅 <liyushen@beisen.com>
2019-06-11 14:21:33 +0800

    安全模式

--- prototype/safemode.js
+++ prototype/safemode.js
@@ -1,17 +1,13 @@
 // 安全模式：判断this是否指向当前函数，从而避免忘记添加new关键字时，添加全局变量的问题。
-let Book = function(title, author) {
-    // 判断是否调用方式是否缺少new关键字
-    // new调用时，this指向新创建的对象，其prototype
+var Book = function(title, author) {
+    // new调用时，this指向新创建的实例 this.__proto__ == Book.prototype
     if(this instanceof Book) {
         return Book(title, author)
     }
     return new Book(title, author)
 }
 
-Book = (title, author) => {
-    console.log(this instanceof Book)
-    // if(this instanceof Book) {
-    //     return Book(title, author)
-    // }
-    // return new Book(title, author)
+// 错误示范：箭头函数不能作为构造函数，在new调用时会抛出一个错误
+var Book = (title, author) => {
+    // some code
 }
