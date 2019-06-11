1bb5ab132116f5e6abd28b70167d75664d48382d
李雨珅 <liyushen@beisen.com>
2019-06-11 13:57:42 +0800

    编写readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # js-data-structure-and-algorithm
 数据结构与算法的js实现
+# js中原型链的结构
+![Image text](./static/ings/prototype.jpg)
--- prototype/safemode.js
+++ prototype/safemode.js
@@ -1,1 +1,17 @@
 // 安全模式：判断this是否指向当前函数，从而避免忘记添加new关键字时，添加全局变量的问题。
+let Book = function(title, author) {
+    // 判断是否调用方式是否缺少new关键字
+    // new调用时，this指向新创建的对象，其prototype
+    if(this instanceof Book) {
+        return Book(title, author)
+    }
+    return new Book(title, author)
+}
+
+Book = (title, author) => {
+    console.log(this instanceof Book)
+    // if(this instanceof Book) {
+    //     return Book(title, author)
+    // }
+    // return new Book(title, author)
+}
